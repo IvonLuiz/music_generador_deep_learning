c868f3c83e610cb5193a2bdb88e264ab13aef0ad
IvonLuiz <ivonluizgarcia@gmail.com>
2024-09-30 14:10:07 -0300

    loader

---
+++ src/processing/preprocess.py
@@ -0,0 +1,25 @@
+import librosa
+import numpy as np
+import os
+import pickle
+
+
+
+class Loader:
+    """
+    Loader is responsible for loading an audio file.
+    """
+
+    def __init__(self, sample_rate, duration, mono):
+        self.sample_rate = sample_rate
+        self.duration = duration
+        self.mono = mono
+
+    def load(self, file_path):
+        signal = librosa.load(file_path,
+                              sr=self.sample_rate,
+                              duration=self.duration,
+                              mono=self.mono)[0]
+        return signal
+
+
