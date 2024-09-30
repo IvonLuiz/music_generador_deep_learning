a50b129a52e6f65807e4b78e394dffafc23fa01e
IvonLuiz <ivonluizgarcia@gmail.com>
2024-09-30 17:13:17 -0300

    min max normalizer

--- src/processing/preprocess.py
+++ src/processing/preprocess.py
@@ -1,8 +1,8 @@
+import os 
+import pickle
+from pathlib import Path
 import librosa
 import numpy as np
-import os
-import pickle
-
 
 
 class Loader:
@@ -61,4 +61,23 @@ class LogSpectrogramExtractor:
         spectrogram = np.abs(stft)
         log_spectrogram = librosa.amplitude_to_db(spectrogram)
         return log_spectrogram
+
+
+class MinMaxNormalizer:
+    """MinMaxnormalizer applies min max normalisation to an array."""
+
+    def __init__(self, min_val, max_val):
+        self.min = min_val
+        self.max = max_val
+
+    def normalize(self, array):
+        norm_array = (array - array.min()) / (array.max() - array.min())
+        norm_array = norm_array * (self.max - self.min) + self.min
+        return norm_array
+
+    def denormalize(self, norm_array, original_min, original_max):
+        array = (norm_array - self.min) / (self.max - self.min)
+        array = array * (original_max - original_min) + original_min
+        return array
+
 
