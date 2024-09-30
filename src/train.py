198114de8bdaed609af8e08be862ac6dfba0d5e1
IvonLuiz <ivonluizgarcia@gmail.com>
2024-09-29 23:44:45 -0300

    training VAE

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
 from tensorflow.keras.datasets import mnist
 
 from autoencoder import Autoencoder
+from vae import VAE
 
 LEARNING_RATE = 0.0005
 BATCH_SIZE = 32
-EPOCHS = 20
+EPOCHS = 100
 
 def load_mnist():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -18,22 +19,20 @@ def load_mnist():
 
 
 def train(x_train, learning_rate, batch_size, epochs):
-    autoencoder = Autoencoder(
+    vae = VAE(
         input_shape=(28, 28, 1),
         conv_filters=(32, 64, 64, 64),
         conv_kernels=(3, 3, 3, 3),
         conv_strides=(1, 2, 2, 1),
         latent_space_dim=2
     )
-    autoencoder.summary()
-    autoencoder.compile(learning_rate)
-    autoencoder.train(x_train, batch_size, epochs)
-    return autoencoder
+    # vae.summary()
+    vae.compile(learning_rate)
+    vae.train(x_train, batch_size, epochs)
+    return vae 
 
 
 if __name__ == "__main__":
     x_train, _, _, _ = load_mnist()
-    autoencoder = train(x_train[:500], LEARNING_RATE, BATCH_SIZE, EPOCHS)
-    autoencoder.save("model")
-    autoencoder2 = Autoencoder.load("model")
-    autoencoder2.summary()
+    VAE = train(x_train[:10000], LEARNING_RATE, BATCH_SIZE, EPOCHS)
+    VAE.save("model")
