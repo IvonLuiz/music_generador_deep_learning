c873fd092eb9e2bc12a7f4cc16c29d2dafc57f36
IvonLuiz <ivonluizgarcia@gmail.com>
2024-10-02 14:16:25 -0300

    full script for generating audio files from vae

--- src/generate.py
+++ src/generate.py
@@ -1,8 +1,18 @@
 import os
+import pickle
 from pathlib import Path
 import numpy as np
 import soundfile as sf
 
+from soundgenerator import SoundGenerator
+from vae import VAE
+from train import SPECTROGRAMS_PATH, load_fsdd
+
+
+HOP_LENGTH = 256
+SAVE_DIR_ORIGINAL = "samples/original/"
+SAVE_DIR_GENERATED = "samples/generated/"
+MIN_MAX_VALUES_PATH = "./data/fsdd/min_max_values.pkl"
 
 
 def select_spectrograms(spectrograms, file_paths, min_max_values, num_spectrograms=2):
@@ -24,3 +34,24 @@ def save_signals(signals, save_dir, sample_rate=22050):
     for i, signal in enumerate(signals):
         save_path = os.path.join(save_dir, str(i) + ".wav")
         sf.write(save_path, signal, sample_rate)
+
+
+if __name__ == "__main__":
+    vae = VAE.load("model")
+    sound_generator = SoundGenerator(vae, HOP_LENGTH)
+
+    # Load spectrograms + min max values
+    with open(MIN_MAX_VALUES_PATH, "rb") as f:
+        min_max_values = pickle.load(f)
+    specs, file_paths = load_fsdd(SPECTROGRAMS_PATH)
+
+    # Sample spectrograms + min max values
+    sampled_specs, sampled_min_max_values = select_spectrograms(specs, file_paths, min_max_values, 5)
+
+    # Generate audio for sampled spectrograms
+    signals, _ = sound_generator.generate(sampled_specs, sampled_min_max_values)
+
+    original_signals = sound_generator.convert_spectrograms_to_audio(sampled_specs, sampled_min_max_values)
+
+    save_signals(signals, SAVE_DIR_GENERATED)
+    save_signals(original_signals, SAVE_DIR_ORIGINAL)
