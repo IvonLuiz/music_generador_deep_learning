5784522894198135db9393116742df46b4406981
IvonLuiz <ivonluizgarcia@gmail.com>
2024-10-21 02:31:29 -0300

    transpose method and run method initial

--- src/processing/preprocess_midi.py
+++ src/processing/preprocess_midi.py
@@ -6,11 +6,14 @@ DATASET_PATH = current_path + "/../../data/raw/maestro-v3.0.0/2018"
 m21.environment.set('musicxmlPath', 'C:/Program Files/MuseScore 4/bin/MuseScore4.exe')
 m21.environment.set('midiPath', 'C:/Program Files/MuseScore 4/bin/MuseScore4.exe')  # Atualize para o caminho correto do MuseScore
 ACCEPTABLE_DURATIONS = [
-    0.25,
-    0.5,
-    0.75,
+    0.25,   # sixteenth note 
+    0.5,    # eighth note
+    0.75,   # dotted eighth note (eighth note + sixteenth note)
     1.0,    # quarter note
-    1.5,    # dotted quarted note
+    1.5,    # dotted quarter note (quarter note + dotted eighth note)
+    2,      # half note
+    3,      # 3 quarter notes
+    4       # whole note
 ]
 
 
@@ -21,6 +24,23 @@ class ProcessingPipeline():
         self.acceptable_durations = acceptable_durations 
 
 
+    def run(self, dataset_path):
+        print("Loading songs...")
+        self.load_songs(dataset_path)
+        print(f"Loaded {len(self.songs)} songs.")
+
+        for song_idx, song in enumerate(self.songs):
+            # Filter out songs if wanted
+            if not self.__has_acceptable_durations(song):
+                continue
+
+            # Transpose songs to Cmaj/Amin
+            song_transposed = self.__transpose_song(song)
+            self.songs[song_idx] = song_transposed 
+
+            # encode songs with music time series representation
+
+            # save songs to text file
 
 
     def load_songs(self, dataset_path):
@@ -32,12 +52,10 @@ class ProcessingPipeline():
                 if file.endswith(('.midi', '.mid')):
                     print(file)
                     song = m21.converter.parse(os.path.join(path, file))
-                    truncated_song = song.measures(0, 10)
+                    truncated_song = song.measures(0, 16)
                     self.songs.append(truncated_song)
-                    print(song)
-                    self.__has_acceptable_durations(song)
                     
-                    if (len(self.songs) > 5):
+                    if (len(self.songs) > 10):
                         break
     
 
@@ -45,16 +63,27 @@ class ProcessingPipeline():
         if self.acceptable_durations == []:
             return True
 
-        for note in song.flat.notesAndRests:
+        for note in song.flatten().notesAndRests:
             if note.duration.quarterLength not in self.acceptable_durations:
                 return False
 
         return True
 
+    def __transpose_song(self, song: m21.stream.Score):
+        key = song.analyze('key')
+        
+        # Transpose to C except if is minor key
+        i = m21.interval.Interval(key.tonic, m21.pitch.Pitch('C'))
+        if key.mode == "minor":
+            i = m21.interval.Interval(key.tonic, m21.pitch.Pitch('A'))
+
+        song_new = song.transpose(i)
+        
+        return song_new
+    
 
     def get_songs(self):
         return self.songs
-
 
     def set_acceptable_durations(self, durations):
         self.acceptable_durations = durations
@@ -62,9 +91,18 @@ class ProcessingPipeline():
 
 if __name__ == "__main__":
     p = ProcessingPipeline()
-    print(DATASET_PATH)
-    p.load_songs(DATASET_PATH)
+    p.run(DATASET_PATH)
+#     print(DATASET_PATH)
+#     p.load_songs(DATASET_PATH)
+#     # p.set_acceptable_durations(ACCEPTABLE_DURATIONS)
+
     songs = p.get_songs()
-    
-    song = songs[1]
-    song.show()
+    song = songs[0]
+#     for song in songs:
+#         print(f"Has acceptable duration? {p.has_acceptable_durations(song)}")
+#         if p.has_acceptable_durations(song)==True:
+    # song.show()
+
+# # MIDI-Unprocessed_Recital1-3_MID--AUDIO_02_R1_2018_wav--3.midi
+# # MIDI-Unprocessed_Recital1-3_MID--AUDIO_02_R1_2018_wav--4.midi
+# # MIDI-Unprocessed_Recital1-3_MID--AUDIO_03_R1_2018_wav--1.midi
