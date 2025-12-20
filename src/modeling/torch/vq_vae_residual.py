1440e06bbfef3a5ce5a588d0a85542a0a94fb775
IvonLuiz <ivonluizgarcia@gmail.com>
2025-12-20 17:36:22 -0300

    fixing inplace operation error during runtime by chaging relu

--- src/modeling/torch/vq_vae_residual.py
+++ src/modeling/torch/vq_vae_residual.py
@@ -10,12 +10,12 @@ class ResidualLayer(nn.Module):
     def __init__(self, in_channels, num_hiddens, num_residual_hiddens):
         super().__init__()
         self._block = nn.Sequential(
-            nn.ReLU(True),
+            nn.ReLU(inplace=False),
             nn.Conv2d(in_channels=in_channels,
                       out_channels=num_residual_hiddens,
                       kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(num_residual_hiddens),
-            nn.ReLU(True),
+            nn.ReLU(inplace=False),
             nn.Conv2d(in_channels=num_residual_hiddens,
                       out_channels=num_hiddens,
                       kernel_size=1, stride=1, bias=False),
