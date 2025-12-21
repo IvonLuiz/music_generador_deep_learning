00a09228031e6e981a29b931f743ca6216d3d95e
IvonLuiz <ivonluizgarcia@gmail.com>
2025-12-21 14:09:06 -0300

    fixing plots and audio generation during training

--- src/train_scripts/train_vqvae_hierarquical.py
+++ src/train_scripts/train_vqvae_hierarquical.py
@@ -3,6 +3,7 @@ import torch
 import os
 import yaml
 import sys
+import pickle
 
 # Add 'src' to sys.path to allow imports from sibling directories
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -11,7 +12,7 @@ from modeling.torch.vq_vae_hierarchical import VQ_VAE_Hierarchical
 from generation.generate import *
 from utils import load_maestro, load_config, initialize_vqvae_hierarchical_model
 from train_scripts.train_vqvae2_utils import train_vqvae_hierarquical
-from processing.preprocess_audio import TARGET_TIME_FRAMES
+from processing.preprocess_audio import TARGET_TIME_FRAMES, MIN_MAX_VALUES_SAVE_DIR
 
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
@@ -62,8 +63,13 @@ if __name__ == "__main__":
         yaml.dump(config, f)
     
     # Load training data to compute variance
-    x_train, _ = load_maestro(spectrograms_path, TARGET_TIME_FRAMES, debug_print=False)
+    x_train, file_paths = load_maestro(spectrograms_path, TARGET_TIME_FRAMES, debug_print=False)
     data_variance = np.var(x_train)
+
+    # Load min_max_values
+    min_max_values_path = os.path.join(MIN_MAX_VALUES_SAVE_DIR, "min_max_values.pkl")
+    with open(min_max_values_path, "rb") as f:
+        min_max_values = pickle.load(f)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     vqvae_hierarquical_model = initialize_vqvae_hierarchical_model(config['model'], device)
@@ -72,6 +78,8 @@ if __name__ == "__main__":
     train_vqvae_hierarquical(
         model=vqvae_hierarquical_model,
         x_train=x_train,
+        train_file_paths=file_paths,
+        min_max_values=min_max_values,
         device=device,
         batch_size=batch_size,
         learning_rate=learning_rate,
