3dbe1b21dee1c9f2c47e68e812385d1c460c3049
IvonLuiz <ivonluizgarcia@gmail.com>
2026-02-19 16:21:41 -0300

    initial refactoring

--- src/modeling/torch/vq_vae_hierarchical.py
+++ src/modeling/torch/vq_vae_hierarchical.py
@@ -4,71 +4,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .vq_vae_residual import ResidualStack
+from .encoder import EncoderBlock
+from .decoder import DecoderBlock
+from .residual_stack import ResidualStack
 from modeling.torch.vector_quantizer import VectorQuantizer
-
-class EncoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, num_residual_layers, stride, kernel_size=4, padding=1):
-        super().__init__()
-        layers = []
-        stride = (stride, stride) if isinstance(stride, int) else stride  # normalize stride to tuple if it's an int
-
-        # Downsampling conv
-        # 4x4 conv with stride 2 is standard for VQ-VAE to reduce checkerboard artifacts
-        layers.append(nn.Conv2d(in_channels,
-                                out_channels,
-                                kernel_size=kernel_size,
-                                stride=stride,
-                                padding=padding))
-        layers.append(nn.BatchNorm2d(out_channels))
-        layers.append(nn.ReLU(inplace=True))
-
-        # Residual Stack after downsampling
-        layers.append(ResidualStack(in_channels=out_channels,
-                                    num_hiddens=out_channels,
-                                    num_residual_hiddens=out_channels // 2,
-                                    num_residual_layers=num_residual_layers))
-
-        self._net = nn.Sequential(*layers)
-    
-    def forward(self, x):
-        x = self._net(x)
-        return x
-
-
-class DecoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, num_residual_layers, stride, kernel_size=4, padding=1, final_relu=True):
-        super().__init__()
-        layers = []
-        stride = (stride, stride) if isinstance(stride, int) else stride  # normalize stride to tuple if it's an int
-
-        # Residual Stack before upsampling
-        layers.append(ResidualStack(in_channels=in_channels,
-                                    num_hiddens=in_channels,
-                                    num_residual_hiddens=in_channels // 2,
-                                    num_residual_layers=num_residual_layers))
-
-        # Upsampling conv (Transpose Conv)
-        layers.append(nn.ConvTranspose2d(in_channels,
-                                         out_channels,
-                                         kernel_size=kernel_size,
-                                         stride=stride,
-                                         padding=padding))
-        layers.append(nn.BatchNorm2d(out_channels))
-        layers.append(nn.ReLU(inplace=True))
-
-        self._net = nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self._net(x)
-        return x
 
 
 class VQ_VAE_Hierarchical(nn.Module):
     """
     Hierarchical VQ-VAE model with two levels of encoders, decoders, and vector quantizers.
     
-    This implementation on the paper Generating Diverse High-Fidelity Images with VQ-VAE-2 (https://arxiv.org/abs/1906.00446).
+    This implementation on the paper Jukebox: A Generative Model for Music (https://arxiv.org/abs/).
     """
 
     def __init__(self,
