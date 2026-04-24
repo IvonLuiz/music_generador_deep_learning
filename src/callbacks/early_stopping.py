f3d3431edabb0a7c3842710f38fb1ee818d0cf1d
IvonLuiz <ivonluizgarcia@gmail.com>
2026-04-24 01:06:26 -0300

    early stopping initial counter and best score from other training

--- src/callbacks/early_stopping.py
+++ src/callbacks/early_stopping.py
@@ -2,19 +2,21 @@ class EarlyStopping:
     """
     Early stops the training if validation loss doesn't improve after a given patience.
     """
-    def __init__(self, patience=7, verbose=False, delta=0):
+    def __init__(self, patience=7, verbose=False, delta=0, best_score=None, counter=0):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
             verbose (bool): If True, prints a message for each validation loss improvement.
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+            best_score (float): Initial value for best score (useful when resuming).
+            counter (int): Initial value for patience counter (useful when resuming).
         """
         self.patience = patience
         self.verbose = verbose
-        self.counter = 0
-        self.best_score = None
+        self.counter = counter
+        self.best_score = best_score
         self.early_stop = False
-        self.val_loss_min = float('inf')
+        self.val_loss_min = float('inf') if best_score is None else -best_score
         self.delta = delta
 
     def __call__(self, val_loss):
